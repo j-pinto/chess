eeab4bf983839a5f429b0381d73eadbec4b05493
jpinto <jfpinto1989@gmail.com>
2021-03-04 18:14:36 -0500

    Refactor board class

--- lib/board.rb
+++ lib/board.rb
@@ -2,22 +2,24 @@ class Board
   attr_accessor :grid
 
   def initialize(pieces)
+    @pieces = pieces
     @grid = {}
     make()
-    populate(pieces)
   end
 
   private
 
-  def make()
+  def make
     half_array = (0..7).to_a
     full_array = half_array.repeated_permutation(2).to_a
 
-    full_array.each { |item| @grid[item] = nil }
+    full_array.each { |location| @grid[location] = nil }
+
+    populate()
   end
 
-  def populate(pieces)
-    pieces.each do |piece|
+  def populate
+    @pieces.each do |piece|
       location = piece.default_position
       @grid[location] = piece
     end
