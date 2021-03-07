0f82b8031db73231012e15ff1eec07df0e61d9a5
jpinto <jfpinto1989@gmail.com>
2021-03-07 15:47:54 -0500

    Add reachable locations method for pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,6 +7,26 @@ class Pawn < Piece
     @capture_moveset = get_pawn_capture_moveset(self)
   end
 
+  def update_reachable_locations(board)
+    reachable_locations = []
+    move = @moveset[0]
+    square = @location.clone
+  
+    loops_number = self.has_moved ? 1 : 2
+    loops_number.times {
+      square[0] += move[0]
+      square[1] += move[1]
+  
+      break if board.friendly_occupied?(square, self.color)
+      break if !( board.in_bounds?(square) )
+  
+      reachable_locations.push(square.clone)
+      break if board.enemy_occupied?(square, self.color)
+    }
+  
+    @reachable_locations = reachable_locations
+  end
+
   def get_pawn_capture_moveset(pawn)
     if pawn.color == 'white'
       return [[1, 1], [-1, 1]]
