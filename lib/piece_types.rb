e3d0bea0fe781d48b07c714b672bc0162da6127b
jpinto <jfpinto1989@gmail.com>
2021-03-07 14:17:07 -0500

    Add version of reachable locations for king&knight

--- lib/piece_types.rb
+++ lib/piece_types.rb
@@ -1,15 +1,34 @@
 class King < Piece
   attr_accessor :in_check
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
     @in_check = false
+  end
+
+  public
+
+  def update_reachable_locations(board)
+    reachable_locations = []
+    @moveset.each do |move|
+      square = @location.clone
+      square[0] += move[0]
+      square[1] += move[1]
+
+      next if board.friendly_occupied?(square, self.color)
+      next if !( board.in_bounds?(square) )
+
+      reachable_locations.push(square.clone)
+      next if board.enemy_occupied?(square, self.color)
+    end
+
+    @reachable_locations = reachable_locations
   end
 end
 
 class Pawn < Piece
   attr_reader :capture_moveset
   attr_accessor :en_pass_vulnerable
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
     @en_pass_vulnerable = false
     @capture_moveset = get_pawn_capture_moveset(self)
@@ -27,25 +46,44 @@ class Pawn < Piece
 end
 
 class Rook < Piece
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
   end
 end
 
 class Bishop < Piece
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
   end
 end
 
 class Queen < Piece
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
   end
 end
 
 class Knight < Piece
-  def initialize(color, default_position=nil)
+  def initialize(color, initial_location)
     super
+  end
+
+  public
+
+  def update_reachable_locations(board)
+    reachable_locations = []
+    @moveset.each do |move|
+      square = @location.clone
+      square[0] += move[0]
+      square[1] += move[1]
+
+      next if board.friendly_occupied?(square, self.color)
+      next if !( board.in_bounds?(square) )
+
+      reachable_locations.push(square.clone)
+      next if board.enemy_occupied?(square, self.color)
+    end
+
+    @reachable_locations = reachable_locations
   end
 end
