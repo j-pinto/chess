9bc6c31f78cf1761205e97572ffdfd53fbbd6ef0
jpinto <jfpinto1989@gmail.com>
2021-03-12 15:05:25 -0500

    Fix input bug with use of instance variable

--- lib/input.rb
+++ lib/input.rb
@@ -1,42 +1,42 @@
 class Input
-  attr_reader :start, :finish
+  attr_reader :start, :finish, :input_string
   def initialize
+    @input_string = nil
     @start = nil
     @finish = nil
   end
   
   def get()
-    input_string = nil
     loop do
       prompt()
-      input_string = gets
-      valid?(input_string) ? break : error_msg()
+      @input_string = gets
+      valid?() ? break : error_msg()
     end
 
-    converted_input = convert(input_string)
+    converted_input = convert()
     @start = converted_input[0]
     @finish = converted_input[1]
   end
 
-  def valid?(string)
-    string = string.chomp.upcase.gsub(/\s+/, "")
-    return false unless string.length == 4
+  def valid?()
+    @input_string = @input_string.chomp.upcase.gsub(/\s+/, "")
+    return false unless @input_string.length == 4
 
-    return false unless string[0].match?(/\A[A-H]*\z/)
-    return false unless string[1].match?(/\A[1-8]*\z/)
+    return false unless @input_string[0].match?(/\A[A-H]*\z/)
+    return false unless @input_string[1].match?(/\A[1-8]*\z/)
 
-    return false unless string[2].match?(/\A[A-H]*\z/)
-    return false unless string[3].match?(/\A[1-8]*\z/)
+    return false unless @input_string[2].match?(/\A[A-H]*\z/)
+    return false unless @input_string[3].match?(/\A[1-8]*\z/)
     
     return true
   end
 
-  def convert(string)
-    start_file = (string[0].ord) % 65
-    start_rank = (string[1].to_i) - 1
+  def convert()
+    start_file = (@input_string[0].ord) % 65
+    start_rank = (@input_string[1].to_i) - 1
 
-    finish_file = (string[2].ord) % 65
-    finish_rank = (string[3].to_i) - 1
+    finish_file = (@input_string[2].ord) % 65
+    finish_rank = (@input_string[3].to_i) - 1
 
     return [ [start_file, start_rank,] ,  [finish_file, finish_rank] ]
   end
