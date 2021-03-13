3b68eff8d7612c250e056c3425c105bcdb0d8709
jpinto <jfpinto1989@gmail.com>
2021-03-12 21:16:46 -0500

    Add more turn switching tests, all pass

--- spec/turn_test.rb
+++ spec/turn_test.rb
@@ -23,4 +23,152 @@ describe Turn do
       game.turn_count += 1
     end
   end
+
+  it 'switches current player according to turn count, leads to selector.output == STANDARD for series of valid inputs corresponding to standard moves' do
+    game = Game.new()
+    board = Board.new()
+    players = {
+      'white' => Player.new('white'),
+      'black' => Player.new('black')
+    }
+
+    game.board = board
+    game.players = players
+
+    puts ""
+    mock_starts = [
+      [0,1],[0,6],[1,0],[1,7]
+    ]
+    mock_finishes = [
+      [0,2],[0,5],[2,2],[2,5]
+    ]
+
+    loop do
+      turn = Turn.new(game)
+      if game.turn_count.even?
+        expect(turn.prompt()).to eql("White To Move")
+      else
+        expect(turn.prompt()).to eql("Black To Move")
+      end
+
+      input = double('input')
+      start = mock_starts.slice!(0)
+      finish = mock_finishes.slice!(0)
+      allow(input).to receive(:start) {start}
+      allow(input).to receive(:finish) {finish}
+
+      allow(turn).to receive(:input) {input}
+      puts "#{turn.input.start}"
+      puts "#{turn.input.finish}"
+
+      selector = MoveTypeSelector.new(turn)
+      selector.set_output()
+      expect(selector.output).to eql('STANDARD')
+
+      game.turn_count += 1
+      break if game.turn_count == 4
+    end
+  end
+
+  it 'switches current player according to turn count, leads to correct selector output, including INVALID if input start occupied by piece of wrong color' do
+    game = Game.new()
+    board = Board.new()
+    players = {
+      'white' => Player.new('white'),
+      'black' => Player.new('black')
+    }
+
+    game.board = board
+    game.players = players
+
+    puts ""
+    mock_starts = [
+      [0,1],[0,6],[1,0],[0,0]
+    ]
+    mock_finishes = [
+      [0,2],[0,5],[2,2],[0,1]
+    ]
+
+    loop do
+      turn = Turn.new(game)
+      if game.turn_count.even?
+        expect(turn.prompt()).to eql("White To Move")
+      else
+        expect(turn.prompt()).to eql("Black To Move")
+      end
+
+      input = double('input')
+      start = mock_starts.slice!(0)
+      finish = mock_finishes.slice!(0)
+      allow(input).to receive(:start) {start}
+      allow(input).to receive(:finish) {finish}
+
+      allow(turn).to receive(:input) {input}
+      puts "#{turn.input.start}"
+      puts "#{turn.input.finish}"
+
+      selector = MoveTypeSelector.new(turn)
+      selector.set_output()
+
+      if game.turn_count == 3
+        expect(selector.output).to eql('INVALID')
+      else
+        expect(selector.output).to eql('STANDARD')
+      end
+
+      game.turn_count += 1
+      break if game.turn_count == 4
+    end
+  end
+
+  it 'switches current player according to turn count, leads to correct selector output, including INVALID if input selects unreachable finish' do
+    game = Game.new()
+    board = Board.new()
+    players = {
+      'white' => Player.new('white'),
+      'black' => Player.new('black')
+    }
+
+    game.board = board
+    game.players = players
+
+    puts ""
+    mock_starts = [
+      [0,1],[0,6],[1,0],[1,7]
+    ]
+    mock_finishes = [
+      [0,2],[0,5],[2,2],[2,6]
+    ]
+
+    loop do
+      turn = Turn.new(game)
+      if game.turn_count.even?
+        expect(turn.prompt()).to eql("White To Move")
+      else
+        expect(turn.prompt()).to eql("Black To Move")
+      end
+
+      input = double('input')
+      start = mock_starts.slice!(0)
+      finish = mock_finishes.slice!(0)
+      allow(input).to receive(:start) {start}
+      allow(input).to receive(:finish) {finish}
+
+      allow(turn).to receive(:input) {input}
+      puts "#{turn.input.start}"
+      puts "#{turn.input.finish}"
+
+      selector = MoveTypeSelector.new(turn)
+      selector.set_output()
+
+      if game.turn_count == 3
+        expect(selector.output).to eql('INVALID')
+      else
+        expect(selector.output).to eql('STANDARD')
+      end
+
+      game.turn_count += 1
+      break if game.turn_count == 4
+    end
+  end
 end
