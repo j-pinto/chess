65179aa4dd451746de797d8c3b5509c885bc682f
jpinto <jfpinto1989@gmail.com>
2021-03-13 10:26:31 -0500

    Add a turn switching tests, passes

--- spec/turn_test.rb
+++ spec/turn_test.rb
@@ -14,6 +14,7 @@ describe Turn do
 
     puts ""
     until game.turn_count == 10
+      puts "count: #{game.turn_count}"
       turn = Turn.new(game)
       if game.turn_count.even?
         expect(turn.prompt()).to eql("White To Move")
@@ -44,6 +45,7 @@ describe Turn do
     ]
 
     loop do
+      puts "count: #{game.turn_count}"
       turn = Turn.new(game)
       if game.turn_count.even?
         expect(turn.prompt()).to eql("White To Move")
@@ -90,6 +92,7 @@ describe Turn do
     ]
 
     loop do
+      puts "count: #{game.turn_count}"
       turn = Turn.new(game)
       if game.turn_count.even?
         expect(turn.prompt()).to eql("White To Move")
@@ -141,6 +144,7 @@ describe Turn do
     ]
 
     loop do
+      puts "count: #{game.turn_count}"
       turn = Turn.new(game)
       if game.turn_count.even?
         expect(turn.prompt()).to eql("White To Move")
@@ -171,4 +175,55 @@ describe Turn do
       break if game.turn_count == 4
     end
   end
+
+  it 'switches current player according to turn count, but only increments turn count if inputs are a valid move' do
+    game = Game.new()
+    board = Board.new()
+    players = {
+      'white' => Player.new('white'),
+      'black' => Player.new('black')
+    }
+
+    game.board = board
+    game.players = players
+
+    puts ""
+    mock_starts = [
+      [0,1],[3,0],[0,1],[0,6]
+    ]
+    mock_finishes = [
+      [0,4],[4,1],[0,3],[0,4]
+    ]
+
+    loop do
+      expect(game.turn_count).to eql(0)
+
+      turn = Turn.new(game)
+      if game.turn_count.even?
+        expect(turn.prompt()).to eql("White To Move")
+      else
+        expect(turn.prompt()).to eql("Black To Move")
+      end
+
+      input = double('input')
+      start = mock_starts.slice!(0)
+      finish = mock_finishes.slice!(0)
+      allow(input).to receive(:start) {start}
+      allow(input).to receive(:finish) {finish}
+
+      allow(turn).to receive(:input) {input}
+      puts "#{turn.input.start}"
+      puts "#{turn.input.finish}"
+
+      selector = MoveTypeSelector.new(turn)
+      selector.set_output()
+
+      if selector.output == 'STANDARD'
+        game.turn_count += 1        
+      end
+
+      break if game.turn_count == 1
+    end
+  end
+  
 end
