ced9f596b6e4804bf0f2ef23c7c7a8a2c189a536
jpinto <jfpinto1989@gmail.com>
2021-03-13 19:58:53 -0500

    Add test for case when player picks empty start

--- spec/turn_test.rb
+++ spec/turn_test.rb
@@ -124,6 +124,58 @@ describe Turn do
     end
   end
 
+  it 'switches current player according to turn count, leads to correct selector output, including INVALID if input start is empty' do
+    game = Game.new()
+    board = Board.new()
+    players = {
+      'white' => Player.new('white'),
+      'black' => Player.new('black')
+    }
+
+    game.board = board
+    game.players = players
+
+    puts ""
+    mock_starts = [
+      [0,1],[0,6],[1,0],[0,4]
+    ]
+    mock_finishes = [
+      [0,2],[0,5],[2,2],[0,1]
+    ]
+
+    loop do
+      puts "count: #{game.turn_count}"
+      turn = Turn.new(game)
+      if game.turn_count.even?
+        expect(turn.prompt()).to eql("White To Move")
+      else
+        expect(turn.prompt()).to eql("Black To Move")
+      end
+
+      input = double('input')
+      start = mock_starts.slice!(0)
+      finish = mock_finishes.slice!(0)
+      allow(input).to receive(:start) {start}
+      allow(input).to receive(:finish) {finish}
+
+      allow(turn).to receive(:input) {input}
+      puts "#{turn.input.start}"
+      puts "#{turn.input.finish}"
+
+      selector = MoveTypeSelector.new(turn)
+      selector.set_output()
+
+      if game.turn_count == 3
+        expect(selector.output).to eql('INVALID')
+      else
+        expect(selector.output).to eql('STANDARD')
+      end
+
+      game.turn_count += 1
+      break if game.turn_count == 4
+    end
+  end
+
   it 'switches current player according to turn count, leads to correct selector output, including INVALID if input selects unreachable finish' do
     game = Game.new()
     board = Board.new()
@@ -225,5 +277,5 @@ describe Turn do
       break if game.turn_count == 1
     end
   end
-  
+
 end
