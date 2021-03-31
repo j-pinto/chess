2c13dd9f8c763d1c86e687cf6ca5f6aedda18a87
jpinto <jfpinto1989@gmail.com>
2021-03-31 16:20:37 -0400

    Edit autoplay to handle missing/unusable test file

--- spec/autoplay/autoplay_spec.rb
+++ spec/autoplay/autoplay_spec.rb
@@ -16,31 +16,42 @@ require './lib/required_files.rb'
 require './spec/autoplay/autoplay_helper.rb'
 
 file = './spec/autoplay/autoplay_test_games.txt'
-matches = AutoPlay.get_matches(file)
-test_count = 0
-failure_count = 0
+if File.exist?(file)
+  matches = AutoPlay.get_matches(file)
+  test_count = 0
+  failure_count = 0
 
-describe AutoPlay do
-  it 'completes each move of a match without error' do
-
-    matches.each { |match|
-      outcome = AutoPlay.success?(match)
+  if matches.length > 0
     
-      if outcome == false
-        failure_count += 1
-      end
+    describe AutoPlay do
+      it 'completes each move of a match without error' do
 
-      test_count += 1
-      print `clear`
-      puts "Starting autoplay test, please wait..."
-      puts "#{matches.size} matches to test"
-      puts "#{test_count} out of #{matches.size} tests complete"
-      puts "#{failure_count} failures"
-    }
-  
-    puts "#{matches.size - failure_count} matches tested successfully"
-    expect(failure_count).to eql(0)
+        matches.each { |match|
+          outcome = AutoPlay.success?(match)
+        
+          if outcome == false
+            failure_count += 1
+          end
+
+          test_count += 1
+          print `clear`
+          puts "Starting autoplay test, please wait..."
+          puts "#{matches.size} matches to test"
+          puts "#{test_count} out of #{matches.size} tests complete"
+          puts "#{failure_count} failures"
+        }
+      
+        puts "#{matches.size - failure_count} matches tested successfully"
+        expect(failure_count).to eql(0)
+      end
+    end
+
+  else
+    puts "Warning: autoplay test could not initiate because text file contains no games"
   end
+
+else
+  puts "Warning: autoplay test could not initiate due to missing or incorrectly formatted game file"
 end
 
 
